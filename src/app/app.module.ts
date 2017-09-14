a24d7c39716730e857281798c5b6432654f8b48b
IgorMarshkov <marshkov@gmail.com>
2017-09-14 12:51:15 +0300

    Add main components with routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ClinicComponent } from './components/clinic/clinic.component';
+import { TherapistComponent } from './components/therapist/therapist.component';
+import { PatientComponent } from './components/patient/patient.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+const routes: Routes = [
+  {
+    'path': '',
+    'redirectTo': 'clinic',
+    'pathMatch': 'full'
+  }, {
+    "path": "clinic",
+    "component": ClinicComponent
+  }, {
+    'path': 'therapist',
+    'component': TherapistComponent
+  }, {
+    'path': 'patient',
+    'component': PatientComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ClinicComponent,
+    TherapistComponent,
+    PatientComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [
 
---
+++ src/app/components/clinic/clinic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clinic',
+  templateUrl: './clinic.component.html',
+  styleUrls: ['./clinic.component.scss']
+})
+
+export class ClinicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
